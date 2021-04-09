88ff98d9c292095ca2b78d4f130b5a7f1c1571e8
Frosenow <filip.potepa@gmail.com>
2021-04-09 22:59:25 +0200

    Extraction Zone Update

--- Intermediate/Build/Win64/TheMazeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Intermediate/Build/Win64/TheMazeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.28.29910
+// Compiler: 14.28.29913
 #include "C:/1 Projekt Obiektowe/TheMazeGame/TheMaze/TheMaze/Intermediate/Build/Win64/TheMazeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Source/TheMaze/Private/FPSExtractionZone.cpp
+++ Source/TheMaze/Private/FPSExtractionZone.cpp
@@ -1,5 +1,6 @@
 #include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
+#include "Characters/CFPSCharacter.h"
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
@@ -37,7 +38,13 @@ void AFPSExtractionZone::Tick(float DeltaTime)
 
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	// Send output log if actor overlap with BoxZone 
-	UE_LOG(LogTemp, Log, TEXT("Pawn overlapped with extractione zone"));
+	
+
+	ACFPSCharacter* MyPawn = Cast<ACFPSCharacter>(OtherActor);
+	if (MyPawn && MyPawn->bIsCarryingObjective)
+	{
+		// Send output log if actor overlap with BoxZone and is carrying an objective
+		UE_LOG(LogTemp, Log, TEXT("Pawn overlapped with extractione zone"));
+	}
 }
 
